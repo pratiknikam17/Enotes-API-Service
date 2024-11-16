fd78cc1a3d9863eaef924c9b85729b9282d7bb6a
Pratik Nikam <pratiknikam547@gmail.com>
2024-11-16 23:48:24 +0530

    initial commit

---
+++ src/main/java/com/pratik/EnotesApiServicessApplication.java
@@ -0,0 +1,13 @@
+package com.pratik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnotesApiServicessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnotesApiServicessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pratik/EnotesApiServicessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnotesApiServicessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
